ad819a215d8e1e04aa94207cf01b5d53a4f5764c
Matthieu F <fallourd.matthieu@gmail.com>
2020-02-05 11:00:05 +0100

    sql clean

--- crebas_SO-Emballage.sql
+++ crebas_SO-Emballage.sql
@@ -1,17 +1,10 @@
 /* ============================================================= */
 /* suppression du compte utilisateur so_emballage		 */
 /*      ignorez l'ereur si celui-ci n'existait pas 		 */
-/* création du compte utilisateur so_emballage			 */
+/* crï¿½ation du compte utilisateur so_emballage			 */
 /* attribution de droits au compte utilisateur so_emballage	 */
 /* connexion avec le compte utilisateur so_emballage		 */
 /* ============================================================= */
-
-drop user so_emballage cascade;
-
-create user so_emballage identified by so_emballage;
-grant connect, resource to so_emballage;
-
-connect so_emballage/so_emballage
 
 
 /*==============================================================*/
@@ -220,84 +213,3 @@ create table PROPOSER
    NO_FOURNISSEUR       NUMBER(3)            not null,
    constraint PK_PROPOSER primary key (CODE_PRODUIT, NO_FOURNISSEUR)
 );
-
-alter table ACHAT
-   add constraint FK_ACHAT_ENVOYER_FOURNISS foreign key (NO_FOURNISSEUR)
-      references FOURNISSEUR (NO_FOURNISSEUR);
-
-alter table CATEGORIE
-   add constraint FK_CATEGORI_EST_CONTE_CATEGORI foreign key (CAT_NO_CATEGORIE)
-      references CATEGORIE (NO_CATEGORIE);
-
-alter table COMMANDE
-   add constraint FK_COMMANDE_PASSER_CLIENT foreign key (NO_CLIENT)
-      references CLIENT (NO_CLIENT);
-
-alter table COMPOSER
-   add constraint FK_COMPOSER_COMPOSER_PRODUIT foreign key (CODE_PRODUIT)
-      references PRODUIT (CODE_PRODUIT);
-
-alter table COMPOSER
-   add constraint FK_COMPOSER_COMPOSER2_MATIERE foreign key (NO_MATIERE)
-      references MATIERE (NO_MATIERE);
-
-alter table DETAIL_ACHAT
-   add constraint FK_DETAIL_A_DETAIL_AC_PRODUIT foreign key (CODE_PRODUIT)
-      references PRODUIT (CODE_PRODUIT);
-
-alter table DETAIL_ACHAT
-   add constraint FK_DETAIL_A_DETAIL_AC_ACHAT foreign key (NO_ACHAT)
-      references ACHAT (NO_ACHAT);
-
-alter table DETAIL_COMMANDE
-   add constraint FK_DETAIL_C_DETAIL_CO_PRODUIT foreign key (CODE_PRODUIT)
-      references PRODUIT (CODE_PRODUIT);
-
-alter table DETAIL_COMMANDE
-   add constraint FK_DETAIL_C_DETAIL_CO_COMMANDE foreign key (NO_COMMANDE)
-      references COMMANDE (NO_COMMANDE);
-
-alter table DETAIL_LIVR_C
-   add constraint FK_DETAIL_L_DETAIL_LI_PRODUIT foreign key (CODE_PRODUIT)
-      references PRODUIT (CODE_PRODUIT);
-
-alter table DETAIL_LIVR_C
-   add constraint FK_DETAIL_L_DETAIL_LI_LIVRAISO foreign key (NO_LIVRAISON_C)
-      references LIVRAISON_C (NO_LIVRAISON_C);
-
-alter table DETAIL_LIVR_F
-   add constraint FK_DETAIL_L_DETAIL_LI_PRODUIT foreign key (CODE_PRODUIT)
-      references PRODUIT (CODE_PRODUIT);
-
-alter table DETAIL_LIVR_F
-   add constraint FK_DETAIL_L_DETAIL_LI_LIVRAISI foreign key (NO_LIVRAISON_F)
-      references LIVRAISON_F (NO_LIVRAISON_F);
-
-alter table LIVRAISON_F
-   add constraint FK_LIVRAISI_SUIVRE_ACHAT foreign key (NO_ACHAT)
-      references ACHAT (NO_ACHAT);
-
-alter table LIVRAISON_C
-   add constraint FK_LIVRAISO_TRAITER_COMMANDE foreign key (NO_COMMANDE)
-      references COMMANDE (NO_COMMANDE);
-
-alter table MOUVEMENT_STOCK
-   add constraint FK_MOUVEMEN_GENERER_PRODUIT foreign key (CODE_PRODUIT)
-      references PRODUIT (CODE_PRODUIT);
-
-alter table PRODUIT
-   add constraint FK_PRODUIT_APPARTENI_CATEGORI foreign key (NO_CATEGORIE)
-      references CATEGORIE (NO_CATEGORIE);
-
-alter table PRODUIT
-   add constraint FK_PRODUIT_STOCKER_EMPLACEM foreign key (NO_EMPLACEMENT)
-      references EMPLACEMENT (NO_EMPLACEMENT);
-
-alter table PROPOSER
-   add constraint FK_PROPOSER_PROPOSER_PRODUIT foreign key (CODE_PRODUIT)
-      references PRODUIT (CODE_PRODUIT);
-
-alter table PROPOSER
-   add constraint FK_PROPOSER_PROPOSER2_FOURNISS foreign key (NO_FOURNISSEUR)
-      references FOURNISSEUR (NO_FOURNISSEUR);
-
